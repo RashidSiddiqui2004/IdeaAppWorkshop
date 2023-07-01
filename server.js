7d7d5db717d7aca6c3f10f64cba220914d59c699
Rashid <siddiqui20042007@gmail.com>
2023-07-01 18:02:44 +0530

    structure of the project

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express')
 
+// const serverConfig = require('./configs/server.config');
+
 const app = express();
 //central cratter analogous to securty guard -> app
 
-app.listen(3333, ()=>{
+app.listen(3333, ()=> {
 
-    console.log("server started :)");
-})
+    // console.log(`server started on the PORT number: ${serverConfig.PORT}`);
+    console.log("hello");
+});
+
+
