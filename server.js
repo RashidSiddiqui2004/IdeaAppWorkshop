89098a6f63dd4af0b5da0366555b9f9a4fbc14d1
Rashid <siddiqui20042007@gmail.com>
2023-07-01 17:20:57 +0530

    files

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+
+const app = express();
+//central cratter analogous to securty guard -> app
+
+app.listen(3333, ()=>{
+
+    console.log("server started :)");
+})
